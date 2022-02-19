8180d5a07af29b75d129211d9e8cc3295a3f0982
DavidHZL <hanzldavid707@gmail.com>
2022-02-18 19:42:26 -0600

    Added complete functionality, needs styling

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let myLibrary = [];
 
-function Book(title, author, numOfPages, hasRead) {
+function Book(title, author, numOfPages, hasRead, no) {
     this.title = title;
     this.author = author;
     this.numOfPages = numOfPages;
@@ -10,16 +10,18 @@ function Book(title, author, numOfPages, hasRead) {
 }
 
 function addBookToLibrary() {
+
     var newTitle = document.getElementById('txtTitle').value;
     var newAuthor = document.getElementById('txtAuthor').value;
     var newNumOfPages = document.getElementById('txtPages').value;
     var newHasRead = document.getElementById('chkHasRead').checked;
 
-    var newBook = new Book(newTitle, newAuthor, newNumOfPages, newHasRead);
+    var newBook = new Book(newTitle, newAuthor, newNumOfPages, newHasRead, );
 
     myLibrary.push(newBook);
 
     displayAllBooks();
+    clearTextBoxes();
 }
 
 function displayAllBooks() {
@@ -29,23 +31,67 @@ function displayAllBooks() {
 
     for (let i = 0; i < myLibrary.length; i++){
         var newDiv = document.createElement('div');
+        newDiv.setAttribute('id', i);
         newDiv.classList.add('libCard');
 
         var pTitle = document.createElement('p');
+        pTitle.classList.add('pTitle');
         pTitle.textContent = myLibrary[i].title;
         newDiv.append(pTitle);
 
         var pAuthor = document.createElement('p');
+        pAuthor.classList.add('pAuthor');
         pAuthor.textContent = myLibrary[i].author;
         newDiv.append(pAuthor);
 
         var pPages = document.createElement('p');
+        pPages.classList.add('pPages');
         pPages.textContent = myLibrary[i].numOfPages;
         newDiv.append(pPages);
 
-        var pHasRead = document.createElement('p');
-        pHasRead.textContent = myLibrary[i].hasRead;
-        newDiv.append(pHasRead);
+        //! This starts createing the hasRead switch
+        var switchLabel = document.createElement('label');
+        switchLabel.classList.add('switch');
+
+        var chkBox = document.createElement('input');
+        chkBox.setAttribute('type', 'checkbox');
+        if (myLibrary[i].hasRead === true) {
+            chkBox.checked = true;
+        } else {
+            chkBox.checked = false;
+        }
+        chkBox.addEventListener('click', function() {
+            let label = this.parentElement;
+            let index = label.parentElement.id;
+            
+            if(this.checked === false) {
+                myLibrary[i].hasRead = false;
+            } else if(this.checked === true) {
+                myLibrary[i].hasRead = true;
+            }
+        });
+        switchLabel.append(chkBox);
+
+        var newSpan = document.createElement('span');
+        newSpan.classList.add('slider');
+        newSpan.classList.add('round');
+        switchLabel.append(newSpan);
+
+        newDiv.append(switchLabel);
+        //! This ends creating the hasRead switch
+
+        var delBtn = document.createElement('button');
+        delBtn.classList.add('delBtn');
+        delBtn.textContent = 'Delete Item';
+        newDiv.append(delBtn);
+
+        delBtn.addEventListener('click', function() {
+            let index = this.parentElement.id;
+            myLibrary.splice(index, 1);
+
+            console.log(myLibrary);
+            displayAllBooks();
+        });
 
         library.append(newDiv);
     }
@@ -55,4 +101,12 @@ function clearLibrary() {
     document.getElementById('library').textContent = "";
 
     myLibrary.length = 0;
+    index = 0;
+}
+
+function clearTextBoxes() {
+    document.getElementById('txtTitle').value = '';
+    document.getElementById('txtAuthor').value = '';
+    document.getElementById('txtPages').value = '';
+    document.getElementById('chkHasRead').checked = false;
 }
